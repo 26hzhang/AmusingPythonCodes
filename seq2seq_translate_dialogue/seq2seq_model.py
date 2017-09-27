47434f2e911ee4ff569601dd0a4b8b514d310b26
zhanghao <isaac.changhau@gmail.com>
2017-09-27 17:47:45 +0800

    Commit

--- seq2seq_translate_dialogue/seq2seq_model.py
+++ seq2seq_translate_dialogue/seq2seq_model.py
@@ -14,6 +14,7 @@ However, it is no longer maintained, since the author creates another repository
 and does experiments in that repo. Here is the link: https://github.com/suriyadeepan/practical_seq2seq
 Anyway, we will still use the codes in easy_seq2seq as example.
 Dataset available here: https://github.com/suriyadeepan/datasets/tree/master/seq2seq/cornell_movie_corpus
+Tensorflow URL: https://github.com/tensorflow/models/tree/master/tutorials/rnn/translate
 """
 
 
@@ -73,6 +74,7 @@ class Seq2SeqModel(object):
         # Attention model
         def seq2seq_f(encoder_inputs, decoder_inputs, do_decode):
             '''url: https://www.tensorflow.org/api_docs/python/tf/contrib/legacy_seq2seq/embedding_attention_seq2seq
+               url: https://www.tensorflow.org/tutorials/recurrent
             This model first embeds encoder_inputs by a newly created embedding (of shape [num_encoder_symbols x input_size]). 
             Then it runs an RNN to encode embedded encoder_inputs into a state vector. It keeps the outputs of this RNN at every step to use for attention later. 
             Next, it embeds decoder_inputs by another newly created embedding (of shape [num_decoder_symbols x input_size]). 
@@ -93,11 +95,14 @@ class Seq2SeqModel(object):
             Returns:
                 A tuple of the form (outputs, state), where: outputs: A list of the same length as decoder_inputs of 2D Tensors with shape [batch_size x num_decoder_symbols] containing the generated outputs. state: The state of each decoder cell at the final time-step. It is a 2D Tensor of shape [batch_size x cell.state_size].
             '''
-            return tf.nn.seq2seq.embedding_attention_seq2seq(encoder_inputs, decoder_inputs, cell,
+            return tf.nn.seq2seq.embedding_attention_seq2seq(encoder_inputs,
+                                                             decoder_inputs,
+                                                             cell,  # RNN cell
                                                              num_encoder_symbols=source_vocab_size,
                                                              num_decoder_symbols=target_vocab_size,
-                                                             embedding_size=size, output_projection=output_projection,
-                                                             feed_previous=do_decode)
+                                                             embedding_size=size,
+                                                             output_projection=output_projection,
+                                                             feed_previous=do_decode)  # if True, only the first of decoder_inputs will be used (the "GO" symbol), otherwise, decoder_inputs are used, thus, for training, set do_decode as False, for testing, set as True
 
         # feed data for model
         self.encoder_inputs = []
@@ -105,15 +110,15 @@ class Seq2SeqModel(object):
         self.target_weights = []
         for i in range(buckets[-1][0]):
             self.encoder_inputs.append(tf.placeholder(tf.int32, shape=[None], name='encoder{0}'.format(i)))
-        for i in range(buckets[-1][1] + 1):
+        for i in range(buckets[-1][1] + 1):  # plus 1, since for decoder, the first symbol is <GO>
             self.decoder_inputs.append(tf.placeholder(tf.int32, shape=[None], name='decoder{0}'.format(i)))
             self.target_weights.append(tf.placeholder(tf.float32, shape=[None], name='weight{0}'.format(i)))
 
-        # the value of targets is one status skewing of the decoder
+        # the value of targets is one status skewing of the decoder, ignore <GO> symbol 
         targets = [self.decoder_inputs[i + 1] for i in range(len(self.decoder_inputs) - 1)]
 
         # the output of training model
-        if forward_only:
+        if forward_only:  # testing
             '''url: https://www.tensorflow.org/api_docs/python/tf/contrib/legacy_seq2seq/model_with_buckets
             Create a sequence-to-sequence model with support for bucketing. The seq2seq argument is a function that defines a sequence-to-sequence model,
                 e.g., seq2seq = lambda x, y: basic_rnn_seq2seq( x, y, rnn_cell.GRUCell(24))
@@ -127,31 +132,43 @@ class Seq2SeqModel(object):
                 softmax_loss_function: Function (labels, logits) -> loss-batch to be used instead of the standard softmax (the default if this is None). Note that to avoid confusion, it is required for the function to accept named arguments.
                 per_example_loss: Boolean. If set, the returned loss will be a batch-sized tensor of losses for each sequence in the batch. If unset, it will be a scalar with the averaged loss from all examples.
                 name: Optional name for this operation, defaults to "model_with_buckets".
+            Returns:
+                A tuple of the form (outputs, losses)
+                outputs: The outputs for each bucket, its j'th element consist of a list of 2D Tensors. The shape of output tensors can be either [batch_size x output_size] or [batch_size x num_decoder_symbols] depending on the seq2seq model used. In our model its output tensors is [batch_size x num_decoder_symbols].
+                losses: list of scalar Tensors, representing losses for each bucket, or, if per_example_loss is set, a list of 1D batch_sized float Tensors.
             '''
-            self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(self.encoder_inputs, self.decoder_inputs,
-                                                                         targets, self.target_weights, buckets,
+            self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(self.encoder_inputs,
+                                                                         self.decoder_inputs,
+                                                                         targets,
+                                                                         self.target_weights,
+                                                                         buckets,
                                                                          lambda x, y: seq2seq_f(x, y, True),  # seq2seq model
                                                                          softmax_loss_function=softmax_loss_function)
-            if output_projection is not None:
+            if output_projection is not None:  # apply output projection
                 for b in range(len(buckets)):
                     self.outputs[b] = [
                         tf.matmul(output, output_projection[0]) + output_projection[1]
                         for output in self.outputs[b]
                     ]
-        else:
-            self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(self.encoder_inputs, self.decoder_inputs,
-                                                                         targets, self.target_weights, buckets,
+        else:  # training
+            self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(self.encoder_inputs,
+                                                                         self.decoder_inputs,
+                                                                         targets,
+                                                                         self.target_weights,
+                                                                         buckets,
                                                                          lambda x, y: seq2seq_f(x, y, False),
                                                                          softmax_loss_function=softmax_loss_function)
 
         # Gradients and SGD update operation for training the model.
-        params = tf.trainable_variables()
+        params = tf.trainable_variables()  # get all the trainable variables
         if not forward_only:
             self.gradient_norms = []
             self.updates = []
             opt = tf.train.AdamOptimizer()
             for b in range(len(buckets)):
                 gradients = tf.gradients(self.losses[b], params)
+                # http://blog.csdn.net/u013713117/article/details/56281715
+                # https://www.tensorflow.org/api_docs/python/tf/clip_by_global_norm
                 clipped_gradients, norm = tf.clip_by_global_norm(gradients, max_gradient_norm)
                 self.gradient_norms.append(norm)
                 self.updates.append(opt.apply_gradients(zip(clipped_gradients, params), global_step=self.global_step))
@@ -187,7 +204,7 @@ class Seq2SeqModel(object):
                                                                                              decoder_size))
 
         # feed inputs
-        input_feed = {}
+        input_feed = {}  # feed_dict: encoder_inputs, decoder_inputs, targets
         for l in range(encoder_size):
             input_feed[self.encoder_inputs[l].name] = encoder_inputs[l]
 
@@ -196,9 +213,9 @@ class Seq2SeqModel(object):
             input_feed[self.target_weights[l].name] = target_weights[l]
 
         last_target = self.decoder_inputs[decoder_size].name
-        input_feed[last_target] = np.zeros([self.batch_size], dtype=np.int32)
+        input_feed[last_target] = np.zeros([self.batch_size], dtype=np.int32)  # _PAD
 
-        # output feed
+        # output feed: fetches, 
         if not forward_only:
             output_feed = [self.updates[bucket_id], self.gradient_norms[bucket_id], self.losses[bucket_id]]
         else:
@@ -206,7 +223,7 @@ class Seq2SeqModel(object):
             for l in range(decoder_size):
                 output_feed.append(self.outputs[bucket_id][l])
 
-        outputs = session.run(output_feed, input_feed)
+        outputs = session.run(output_feed, input_feed)  # runs operations and evaluates tensors in fetches
 
         if not forward_only:
             return outputs[1], outputs[2], None  # output for backward: gradient, loss, None
@@ -238,7 +255,7 @@ class Seq2SeqModel(object):
             encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
             # Decoder inputs get an extra "GO" symbol, and are padded then.
             decoder_pad_size = decoder_size - len(decoder_input) - 1
-            decoder_inputs.append([data_utils.GO_ID] + decoder_input + [data_utils.PAD_ID] * decoder_pad_size)
+            decoder_inputs.append([data_utils.GO_ID] + decoder_input + [data_utils.PAD_ID] * decoder_pad_size)  # ? no EOS symbol?
 
         # Now we create batch-major vectors from the data selected above.
         batch_encoder_inputs, batch_decoder_inputs, batch_weights = [], [], []
